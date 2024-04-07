dfd2fdd5bcd78ab2675525933a526fb326fa80d4
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-04-07 16:06:52 +0200

    .

---
+++ _JuezOnline/H - Operacion camello/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> erantzuna = new ArrayList<>();
+
+        //https://ourense.programame.com/team/problems/19/text
+
+
+
+
+
+
+
+
+        int zenbat = in.nextInt();
+        for (int i =0;i<=zenbat;i++){
+
+
+
+
+
+
+        }
+
+        for (String s : erantzuna) {
+            System.out.println(s);
+        }
+    }
+
+
+
+}
