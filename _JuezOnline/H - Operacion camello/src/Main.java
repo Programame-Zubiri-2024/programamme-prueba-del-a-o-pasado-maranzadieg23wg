e0db9ab40110d90ed93668c5afb669c4a9de4e49
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-04-07 17:05:42 +0200

    H eginda

--- _JuezOnline/H - Operacion camello/src/Main.java
+++ _JuezOnline/H - Operacion camello/src/Main.java
@@ -13,12 +13,27 @@ public class Main {
 
 
 
+        int z1 = 0;
+        int z2 = 0;
+        int z3 = 0;
 
+        while (true){
 
-        int zenbat = in.nextInt();
-        for (int i =0;i<=zenbat;i++){
+            z1 = in.nextInt();
+            z2 = in.nextInt();
+            z3 = in.nextInt();
 
+            if (z1 == -1 && z2 == -1 && z3 == -1){
+                break;
+            }
+            int egunak = egun(z1, z2, z3);
 
+            //System.out.println(egunak);
+            if (egunak == -1){
+                erantzuna.add("OLVIDA LOS CAMELLOS");
+            }else {
+                erantzuna.add(String.valueOf(egunak));
+            }
 
 
 
@@ -28,6 +43,17 @@ public class Main {
         for (String s : erantzuna) {
             System.out.println(s);
         }
+    }
+
+    static int egun(int z1, int z2, int z3){
+
+        if (z2<z3){
+            return -1;
+        }
+
+        int z = z2 -z3;
+        return z1 /z;
+
     }
 
 
